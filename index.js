77a14f6c1484791241ea22a696fc7989e7ffe31c
Traae <traaebloxham@gmail.com>
2024-08-24 14:43:27 -0600

    Added convert to normal. Just need a stop watch and we'll have everything.

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-/* USPS Time Tool
+// USPS Time Tool
 
-I'm still new to javascript and how it interacts with html.
-This code has been copied and adapted from:
-https://github.com/jamezmca/calc
-
-*/
-
-// UI Code
-
-// Clock
-// const timeDisplay = document.getElementById("clockValue");
-
-// function updateClockUI() {
-//   const timeDisplay = ;
-//   displayInput.innerText = timeDisplay.trim() ? timeDisplay : "error";
-// }
-
+// CLOCK
 function startTime() {
   var time = new Date();
   var clock = time.toLocaleTimeString("en-US", { hour12: true });
@@ -32,6 +17,7 @@ function startTime() {
 }
 startTime();
 
+// CONVERT TO POSTAL
 function convertToPostalTime(hour, minute) {
   var h = formatNumber(hour);
 
@@ -40,13 +26,6 @@ function convertToPostalTime(hour, minute) {
 
   return h + m;
 }
-function formatNumber(n) {
-  if (n < 10) {
-    return "0" + n;
-  }
-  return "" + n;
-}
-
 function convertToPostalButton() {
   var input = document.getElementById("timeInput");
   var output = convertToPostalTime(
@@ -55,6 +34,112 @@ function convertToPostalButton() {
   );
   document.getElementById("convertToPostal").innerHTML = output;
 }
+function formatNumber(n) {
+  if (n < 10) {
+    return "0" + n;
+  }
+  return "" + n;
+}
+
+// CONVERT TO NORMAL
+function convertToNormalTime(postal) {
+  // expecting a 4 character string representing postal time
+  var t = new Date();
+
+  var h = parseInt(postal.slice(0, 2));
+  var m = parseInt(postal.slice(2, 4));
+
+  t.setHours(h);
+  t.setMinutes(Math.floor((m / 100) * 60));
+
+  return t.toLocaleTimeString("en-US", { hour12: true, timeStyle: "short" });
+}
+function convertToNormalButton() {
+  var input = document.getElementById("postalInput");
+  // input should only be 4 characters
+  document.getElementById("convertToNormal").innerHTML = convertToNormalTime(
+    input.value
+  );
+}
+
+// // STOP WATCH
+// let startBtn = document.getElementById('start');
+// let stopBtn = document.getElementById('stop');
+// let resetBtn = document.getElementById('reset');
+
+// let hour = 00;
+// let minute = 00;
+// let second = 00;
+// let count = 00;
+
+// startBtn.addEventListener('click', function () {
+//     timer = true;
+//     stopWatch();
+// });
+
+// stopBtn.addEventListener('click', function () {
+//     timer = false;
+// });
+
+// resetBtn.addEventListener('click', function () {
+//     timer = false;
+//     hour = 0;
+//     minute = 0;
+//     second = 0;
+//     count = 0;
+//     document.getElementById('hr').innerHTML = "00";
+//     document.getElementById('min').innerHTML = "00";
+//     document.getElementById('sec').innerHTML = "00";
+//     document.getElementById('count').innerHTML = "00";
+// });
+
+// function stopWatch() {
+//     if (timer) {
+//         count++;
+
+//         if (count == 100) {
+//             second++;
+//             count = 0;
+//         }
+
+//         if (second == 60) {
+//             minute++;
+//             second = 0;
+//         }
+
+//         if (minute == 60) {
+//             hour++;
+//             minute = 0;
+//             second = 0;
+//         }
+
+//         let hrString = hour;
+//         let minString = minute;
+//         let secString = second;
+//         let countString = count;
+
+//         if (hour < 10) {
+//             hrString = "0" + hrString;
+//         }
+
+//         if (minute < 10) {
+//             minString = "0" + minString;
+//         }
+
+//         if (second < 10) {
+//             secString = "0" + secString;
+//         }
+
+//         if (count < 10) {
+//             countString = "0" + countString;
+//         }
+
+//         document.getElementById('hr').innerHTML = hrString;
+//         document.getElementById('min').innerHTML = minString;
+//         document.getElementById('sec').innerHTML = secString;
+//         document.getElementById('count').innerHTML = countString;
+//         setTimeout(stopWatch, 10);
+// }
 
 /*
 const displayInput = document.getElementById("inputValue");
